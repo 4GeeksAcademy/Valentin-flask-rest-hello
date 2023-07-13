ae5c33b38e6469f6d29d57fa40cce0ef414c24db
ValentinFrAr <116929850+ValentinFrAr@users.noreply.github.com>
2023-07-12 22:12:56 -0300

    Update admin.py

--- src/admin.py
+++ src/admin.py
@@ -1,21 +1,35 @@
 import os
 from flask_admin import Admin
-from models import db, User, Character, Planet, Favorite
+
 from flask_admin.contrib.sqla import ModelView
+
+from models import db, User, Address, Character, Planet, Vehicle, FavoriteList
+
 
 def setup_admin(app):
     app.secret_key = os.environ.get('FLASK_APP_KEY', 'sample key')
     app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
     admin = Admin(app, name='4Geeks Admin', template_mode='bootstrap3')
 
+    
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(User, db.session))
+
+    # You can duplicate that line to add mew models
+    # admin.add_view(ModelView(YourModelName, db.session))
+
+    
+    # Add Address model to the admin
+    admin.add_view(ModelView(Address, db.session))
+
+     # Add Planet model to the admin
+    admin.add_view(ModelView(Planet, db.session))
 
     # Add Character model to the admin
     admin.add_view(ModelView(Character, db.session))
 
-    # Add Planet model to the admin
-    admin.add_view(ModelView(Planet, db.session))
+    # Add Vehicle model to the admin
+    admin.add_view(ModelView(Vehicle, db.session))
 
-    # Add Favorite model to the admin
-    admin.add_view(ModelView(Favorite, db.session))
+    # Add FavoriteList model to the admin
+    admin.add_view(ModelView(FavoriteList, db.session))
